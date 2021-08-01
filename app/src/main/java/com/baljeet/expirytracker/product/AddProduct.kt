746f9d2a4380b420d77a87a16307a01894a61550
baljeet singh <bsingh@currentstudios.ca>
2021-08-01 01:52:31 -0300

    add product fragment created

---
+++ app/src/main/java/com/baljeet/expirytracker/product/AddProduct.kt
@@ -0,0 +1,23 @@
+package com.baljeet.expirytracker.product
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.baljeet.expirytracker.R
+
+class AddProduct : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_add_product, container, false)
+
+
+
+        return view
+    }
+
+}
