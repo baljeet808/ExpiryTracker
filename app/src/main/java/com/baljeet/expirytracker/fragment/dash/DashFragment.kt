949772ed845f046b4f5068862b658d599d4bf742
baljeet singh <bsingh@currentstudios.ca>
2021-09-07 08:23:53 -0300

    warnings crushed

--- app/src/main/java/com/baljeet/expirytracker/fragment/dash/DashFragment.kt
+++ app/src/main/java/com/baljeet/expirytracker/fragment/dash/DashFragment.kt
@@ -4,13 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.baljeet.expirytracker.R
 import com.baljeet.expirytracker.data.relations.TrackerAndProduct
 import com.baljeet.expirytracker.data.viewmodels.CategoryViewModel
