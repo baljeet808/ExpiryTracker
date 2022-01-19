f6bc289a7563c18c785519f6ea121af0ee2c5616
baljeet singh <bsingh@currentstudios.ca>
2022-01-19 01:01:20 -0400

    category info page updated

--- app/src/main/java/com/baljeet/expirytracker/fragment/settings/categories/CategoryInfo.kt
+++ app/src/main/java/com/baljeet/expirytracker/fragment/settings/categories/CategoryInfo.kt
@@ -5,11 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.baljeet.expirytracker.R
 import com.baljeet.expirytracker.data.viewmodels.CategoryViewModel
 import com.baljeet.expirytracker.databinding.FragmentCategoryInfoBinding
+import com.baljeet.expirytracker.util.ImageConvertor
 
 class CategoryInfo : Fragment() {
 
@@ -26,6 +28,25 @@ class CategoryInfo : Fragment() {
            bind.closeBtn.setOnClickListener { activity?.onBackPressed() }
             val category = navArgs.categoryAndImage
             fragmentTitleText.text = category.category.categoryName
+
+            when(category.image.mimeType){
+                "asset"->{
+                    imageView.setImageDrawable(
+                        AppCompatResources.getDrawable(
+                            requireContext(),
+                            resources.getIdentifier(
+                                category.image.imageUrl,
+                                "drawable",
+                                requireContext().packageName
+                            )
+                        )
+                    )
+                }
+                else->{
+                    imageView.setImageBitmap(ImageConvertor.stringToBitmap(category.image.bitmap))
+                }
+            }
+            nameEdittext.setText(category.category.categoryName)
         }
         return bind.root
     }
