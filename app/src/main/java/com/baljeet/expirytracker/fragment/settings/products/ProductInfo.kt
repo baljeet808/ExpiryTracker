6f0896fc4e98bfc8ae9f04f7b921e231ce4e1b8f
baljeet singh <bsingh@currentstudios.ca>
2022-02-12 02:02:41 -0400

    product icon image editing issue fixed

--- app/src/main/java/com/baljeet/expirytracker/fragment/settings/products/ProductInfo.kt
+++ app/src/main/java/com/baljeet/expirytracker/fragment/settings/products/ProductInfo.kt
@@ -3,29 +3,44 @@ package com.baljeet.expirytracker.fragment.settings.products
 import android.app.AlertDialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.os.Handler
 import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.content.res.AppCompatResources
-import androidx.core.view.isGone
+import androidx.core.content.FileProvider
 import androidx.core.view.setPadding
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
 import com.baljeet.expirytracker.R
+import com.baljeet.expirytracker.data.Image
 import com.baljeet.expirytracker.data.Product
+import com.baljeet.expirytracker.data.viewmodels.ImageViewModel
 import com.baljeet.expirytracker.data.viewmodels.ProductViewModel
 import com.baljeet.expirytracker.databinding.DeletePopUpLayoutBinding
 import com.baljeet.expirytracker.databinding.FragmentProductInfoBinding
+import com.baljeet.expirytracker.databinding.ImagePickingOptionsLayoutBinding
+import com.baljeet.expirytracker.fragment.product.CustomViewModel
+import com.baljeet.expirytracker.fragment.settings.categories.CategoryInfoDirections
+import com.baljeet.expirytracker.fragment.shared.IconsViewModel
 import com.baljeet.expirytracker.util.ImageConvertor
 import com.baljeet.expirytracker.util.SharedPref
+import com.baljeet.expirytracker.util.getContentType
+import com.baljeet.expirytracker.util.getFileName
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import java.io.File
 
 
 class ProductInfo : Fragment() {
@@ -33,6 +48,10 @@ class ProductInfo : Fragment() {
     private lateinit var bind: FragmentProductInfoBinding
     private val viewModel: ProductViewModel by viewModels()
     private val navArgs: ProductInfoArgs by navArgs()
+    private val imageViewModel : ImageViewModel by viewModels()
+    private val customViewModel : CustomViewModel by activityViewModels()
+
+    private val iconViewModel : IconsViewModel by activityViewModels()
 
 
     private lateinit var deleteDialog: AlertDialog
@@ -46,6 +65,63 @@ class ProductInfo : Fragment() {
         }
     }
 
+    private val pickAction = registerForActivityResult(
+        ActivityResultContracts.GetContent()
+    ) { uri ->
+        uri?.let {
+            val image = Image(
+                imageId  = 0,
+                imageUrl = it.toString(),
+                imageName = requireContext().contentResolver.getFileName(it),
+                alt = "image",
+                mimeType = requireContext().contentResolver.getContentType(it),
+                uri = it,
+                bitmap = ""
+            )
+            openEditor(image)
+        }
+    }
+
+    private var latestTmpUri: Uri? = null
+
+    private val takeImageResult =
+        registerForActivityResult(ActivityResultContracts.TakePicture()) { isSuccess ->
+            if (isSuccess) {
+                latestTmpUri?.let { u ->
+                    val image = Image(
+                        imageId = 0,
+                        imageUrl = u.toString(),
+                        imageName = requireContext().contentResolver.getFileName(u),
+                        alt = "image",
+                        mimeType = "image/jpeg",
+                        uri = u ,
+                        bitmap =""
+                    )
+                    openEditor(image)
+                }
+            }
+        }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activity?.onBackPressedDispatcher?.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                clearImageCache()
+                isEnabled = false
+                activity?.onBackPressed()
+
+            }
+        })
+        SharedPref.init(requireContext())
+    }
+
+
+    fun clearImageCache(){
+        iconViewModel.selectedIcon = null
+        customViewModel.croppedImage = null
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -55,25 +131,62 @@ class ProductInfo : Fragment() {
             closeBtn.setOnClickListener { activity?.onBackPressed() }
             val product = navArgs.productAndImage
 
-            when (product.image.mimeType) {
-                "asset" -> {
-                    imageView.setPadding(60)
-                    imageView.setImageDrawable(
-                        AppCompatResources.getDrawable(
-                            requireContext(),
-                            resources.getIdentifier(
-                                product.image.imageUrl,
-                                "drawable",
-                                requireContext().packageName
-                            )
+            iconViewModel.selectedIcon?.let {
+                imageView.setPadding(60)
+                imageView.setImageDrawable(
+                    AppCompatResources.getDrawable(
+                        requireContext(),
+                        resources.getIdentifier(
+                            it.imageUrl,
+                            "drawable",
+                            requireContext().packageName
                         )
                     )
-                }
-                else -> {
-                    imageView.setPadding(0)
-                    imageView.setImageBitmap(ImageConvertor.stringToBitmap(product.image.bitmap))
+                )
+            }?: kotlin.run {
+                customViewModel.croppedImage?.let {
+                    when (it.mimeType) {
+                        "asset" -> {
+                            imageView.setPadding(60)
+                            imageView.setImageDrawable(
+                                AppCompatResources.getDrawable(
+                                    requireContext(),
+                                    resources.getIdentifier(
+                                        it.imageUrl,
+                                        "drawable",
+                                        requireContext().packageName
+                                    )
+                                )
+                            )
+                        }
+                        else -> {
+                            imageView.setPadding(0)
+                            imageView.setImageBitmap(ImageConvertor.stringToBitmap(it.bitmap))
+                        }
+                    }
+                }?: kotlin.run {
+                    when (product.image.mimeType) {
+                        "asset" -> {
+                            imageView.setPadding(60)
+                            imageView.setImageDrawable(
+                                AppCompatResources.getDrawable(
+                                    requireContext(),
+                                    resources.getIdentifier(
+                                        product.image.imageUrl,
+                                        "drawable",
+                                        requireContext().packageName
+                                    )
+                                )
+                            )
+                        }
+                        else -> {
+                            imageView.setPadding(0)
+                            imageView.setImageBitmap(ImageConvertor.stringToBitmap(product.image.bitmap))
+                        }
+                    }
                 }
             }
+
             nameEdittext.setText(product.product.name)
 
             nameEdittext.doOnTextChanged { _, _, _, _ ->
@@ -85,11 +198,21 @@ class ProductInfo : Fragment() {
             }
 
             saveButton.setOnClickListener {
+
+                val imageId = iconViewModel.selectedIcon?.imageId ?: kotlin.run {
+                    customViewModel.croppedImage?.let {
+                        imageViewModel.addImage(it)
+                        imageViewModel.getImageByName(it.imageName).imageId
+                    }  ?: kotlin.run {
+                        product.image.imageId
+                    }
+                }
+
                 viewModel.updateProduct(
                     Product(
                         categoryId = product.product.categoryId,
                         name = nameEdittext.text.toString(),
-                        imageId = product.image.imageId,
+                        imageId = imageId,
                         productId = product.product.productId,
                         isDeleted = false
                     )
@@ -100,8 +223,58 @@ class ProductInfo : Fragment() {
                 showPopup()
             }
 
+            editImageBtn.setOnClickListener {
+                showBottomSheet()
+            }
+
         }
         return bind.root
+    }
+
+
+    private fun openEditor(image : Image){
+        Navigation.findNavController(requireView()).navigate(ProductInfoDirections.actionProductInfoToImageEditor(image))
+    }
+
+
+
+    private fun showBottomSheet() {
+        val bottomSheetDialog = BottomSheetDialog(requireContext(),R.style.BottomSheetDialogTheme)
+        val pickerBind = ImagePickingOptionsLayoutBinding.inflate(layoutInflater)
+        pickerBind.uploadBtn.setOnClickListener {
+            pickAction.launch("image/*")
+            clearImageCache()
+            bottomSheetDialog.dismiss()
+        }
+
+        bottomSheetDialog.setOnDismissListener {
+            bind.editImageBtn.isEnabled = true
+        }
+
+        pickerBind.iconsBtn.setOnClickListener {
+            customViewModel.croppedImage
+            clearImageCache()
+            bottomSheetDialog.dismiss()
+            Navigation.findNavController(requireView()).navigate(ProductInfoDirections.actionProductInfoToIconsGallery())
+        }
+        pickerBind.cameraBtn.setOnClickListener {
+            bottomSheetDialog.dismiss()
+            val photoFile = File.createTempFile(
+                "IMG_",
+                ".jpg",
+                requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+            )
+
+            latestTmpUri = FileProvider.getUriForFile(
+                requireContext(),
+                "${requireContext().packageName}.provider",
+                photoFile
+            )
+            clearImageCache()
+            takeImageResult.launch(latestTmpUri)
+        }
+        bottomSheetDialog.setContentView(pickerBind.root)
+        bottomSheetDialog.show()
     }
 
 
