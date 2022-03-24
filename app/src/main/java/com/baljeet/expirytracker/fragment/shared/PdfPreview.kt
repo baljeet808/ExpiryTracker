1d7a9fe74104566fd3d798ac5f68b1ae083dd6b7
baljeet singh <bsingh@currentstudios.ca>
2022-03-24 17:05:51 -0300

    edit pdf is pro feature now

--- app/src/main/java/com/baljeet/expirytracker/fragment/shared/PdfPreview.kt
+++ app/src/main/java/com/baljeet/expirytracker/fragment/shared/PdfPreview.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.core.view.isGone
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
@@ -40,12 +39,15 @@ class PdfPreview : Fragment() {
             backButton.setOnClickListener { activity?.onBackPressed() }
             SharedPref.init(requireContext())
             SharedPref.isUserAPro.apply {
-                pdfToolsCard.isEnabled = this
-                proIcon.isGone = this
+                blockView.isGone = this
+                proButton.isGone = this
+            }
+            blockView.setOnClickListener {
+               Navigation.findNavController(requireView()).navigate(PdfPreviewDirections.actionPdfPreviewToBePro())
             }
             requestData = args.requestData
 
-            proIcon.setOnClickListener {
+            proButton.setOnClickListener {
                 Navigation.findNavController(requireView())
                     .navigate(PdfPreviewDirections.actionPdfPreviewToBePro())
             }
