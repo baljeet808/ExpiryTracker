2441e89160460ffb2b664f4d82eb8adc8457566e
baljeet singh <bsingh@currentstudios.ca>
2022-04-07 00:49:32 -0300

    prices text are fixed for donate screen

--- app/src/main/java/com/baljeet/expirytracker/fragment/settings/donate/DonateFragment.kt
+++ app/src/main/java/com/baljeet/expirytracker/fragment/settings/donate/DonateFragment.kt
@@ -9,36 +9,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.MutableLiveData
-import com.android.billingclient.api.*
-import com.baljeet.expirytracker.R
+import androidx.fragment.app.activityViewModels
+import com.android.billingclient.api.SkuDetails
 import com.baljeet.expirytracker.databinding.FragmentDonateBinding
 import com.baljeet.expirytracker.databinding.ThanksPopUpLayoutBinding
 import com.baljeet.expirytracker.util.Constants
-import com.baljeet.expirytracker.util.MyColors
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.rewarded.RewardedAd
 import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback
 
 
-enum class DonationOptions {
-    AD, CANDY, CHOCOLATE, COFFEE, BURGER, DINNER, EMPTY
-}
-
 class DonateFragment : Fragment() {
 
     private lateinit var bind: FragmentDonateBinding
 
-    private val viewModel: DonateViewModel by viewModels()
-
-    private var selectedOption: DonationOptions = DonationOptions.EMPTY
-
-    private val optionSelected = MutableLiveData(false)
-
     private var mRewardedAd: RewardedAd? = null
 
+    private val viewModel : DonateViewModel by activityViewModels()
 
     private lateinit var thanksDialog: AlertDialog
     private lateinit var dialogBuilder: AlertDialog.Builder
@@ -52,90 +40,8 @@ class DonateFragment : Fragment() {
         bind.apply {
             backButton.setOnClickListener { activity?.onBackPressed() }
             adCard.setOnClickListener {
-                resetAllButtons()
-                adLayout.setBackgroundColor(
-                    MyColors.getColorByAttr(
-                        requireContext(),
-                        R.attr.card_background,
-                        R.color.tab_background
-                    )
-                )
-                viewModel.selectAd()
-                adCheck.visibility = View.VISIBLE
-                selectedOption = DonationOptions.AD
-                optionSelected.postValue(true)
+                showRewardAD()
             }
-            candyCard.setOnClickListener {
-                resetAllButtons()
-                candyLayout.setBackgroundColor(
-                    MyColors.getColorByAttr(
-                        requireContext(),
-                        R.attr.card_background,
-                        R.color.tab_background
-                    )
-                )
-                viewModel.selectCandy()
-                candyCheck.visibility = View.VISIBLE
-                selectedOption = DonationOptions.CANDY
-                optionSelected.postValue(true)
-            }
-            chocolateCard.setOnClickListener {
-                resetAllButtons()
-                chocolateLayout.setBackgroundColor(
-                    MyColors.getColorByAttr(
-                        requireContext(),
-                        R.attr.card_background,
-                        R.color.tab_background
-                    )
-                )
-                viewModel.selectChocolate()
-                chocolateCheck.visibility = View.VISIBLE
-                selectedOption = DonationOptions.CHOCOLATE
-                optionSelected.postValue(true)
-            }
-            coffeeCard.setOnClickListener {
-                resetAllButtons()
-                coffeeLayout.setBackgroundColor(
-                    MyColors.getColorByAttr(
-                        requireContext(),
-                        R.attr.card_background,
-                        R.color.tab_background
-                    )
-                )
-                viewModel.selectCoffee()
-                coffeeCheck.visibility = View.VISIBLE
-                selectedOption = DonationOptions.COFFEE
-                optionSelected.postValue(true)
-            }
-            burgerCard.setOnClickListener {
-                resetAllButtons()
-                burgerLayout.setBackgroundColor(
-                    MyColors.getColorByAttr(
-                        requireContext(),
-                        R.attr.card_background,
-                        R.color.tab_background
-                    )
-                )
-                viewModel.selectBurger()
-                burgerCheck.visibility = View.VISIBLE
-                selectedOption = DonationOptions.BURGER
-                optionSelected.postValue(true)
-            }
-            mealCard.setOnClickListener {
-                resetAllButtons()
-                mealLayout.setBackgroundColor(
-                    MyColors.getColorByAttr(
-                        requireContext(),
-                        R.attr.card_background,
-                        R.color.tab_background
-                    )
-                )
-                viewModel.selectMeal()
-                mealCheck.visibility = View.VISIBLE
-                selectedOption = DonationOptions.DINNER
-                optionSelected.postValue(true)
-            }
-
             val adRequest = AdRequest.Builder().build()
             RewardedAd.load(requireContext(),
                 Constants.DONATE_REWARDED_AD_ID, adRequest, object : RewardedAdLoadCallback() {
@@ -149,76 +55,12 @@ class DonateFragment : Fragment() {
                     }
                 })
 
-
-            val skuList = ArrayList<String>()
-
-            val purchasesUpdatedListener = PurchasesUpdatedListener { billingResult, purchases ->
-
+            viewModel.skuLiveList.observe(viewLifecycleOwner){
+                 setUpPrices(it)
             }
-
-
-            val billingClient = BillingClient.newBuilder(requireContext())
-                .setListener(purchasesUpdatedListener)
-                .enablePendingPurchases()
-                .build()
-
-
-
-            donateButton.setOnClickListener {
-                when (selectedOption) {
-                    DonationOptions.AD -> {
-                        showRewardAD()
-                    }
-                    DonationOptions.CANDY -> {
-                        skuList.clear()
-                        skuList.add(Constants.CANDY_ID)
-                    }
-                    DonationOptions.CHOCOLATE -> {
-                        skuList.clear()
-                        skuList.add(Constants.CHOCOLATE_ID)
-                    }
-                    DonationOptions.COFFEE -> {
-                        skuList.clear()
-                        skuList.add(Constants.COFFEE_ID)
-                    }
-                    DonationOptions.BURGER -> {
-                        skuList.clear()
-                        skuList.add(Constants.BURGER_ID)
-                    }
-                    DonationOptions.DINNER -> {
-                        skuList.clear()
-                        skuList.add(Constants.DINNER_ID)
-                    }
-                    else -> Unit
-                }
-                if (selectedOption != DonationOptions.AD) {
-
-                    billingClient.startConnection(object : BillingClientStateListener {
-                        override fun onBillingServiceDisconnected() {
-
-                        }
-
-                        override fun onBillingSetupFinished(billingResult: BillingResult) {
-                            if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
-                                val param = SkuDetailsParams.newBuilder()
-                                param.setSkusList(skuList).setType(BillingClient.SkuType.INAPP)
-
-                                billingClient.querySkuDetailsAsync(param.build()) { billingResults, skuDetailsList ->
-
-
-                                    val flowPurchase = BillingFlowParams.newBuilder()
-                                        .setSkuDetails(skuDetailsList!![0])
-                                        .build()
-
-                                    billingClient.launchBillingFlow(
-                                        requireActivity(),
-                                        flowPurchase
-                                    ).responseCode
-
-                                }
-                            }
-                        }
-                    })
+            viewModel.purchaseComplete.observe(viewLifecycleOwner){
+                if(it){
+                    showThankYouPopup()
                 }
             }
         }
@@ -236,6 +78,7 @@ class DonateFragment : Fragment() {
     }
 
 
+
     private fun showThankYouPopup() {
         dialogBuilder = AlertDialog.Builder(requireContext())
         val popUpBinding = ThanksPopUpLayoutBinding.inflate(LayoutInflater.from(requireContext()))
@@ -246,60 +89,47 @@ class DonateFragment : Fragment() {
         thanksDialog.show()
     }
 
-
-    private fun resetAllButtons() {
+    private fun setUpPrices(list : MutableList<SkuDetails>){
         bind.apply {
-            adLayout.setBackgroundColor(
-                MyColors.getColorByAttr(
-                    requireContext(),
-                    R.attr.card_background,
-                    R.color.tab_background
-                )
-            )
-            candyLayout.setBackgroundColor(
-                MyColors.getColorByAttr(
-                    requireContext(),
-                    R.attr.card_background,
-                    R.color.tab_background
-                )
-            )
-            chocolateLayout.setBackgroundColor(
-                MyColors.getColorByAttr(
-                    requireContext(),
-                    R.attr.card_background,
-                    R.color.tab_background
-                )
-            )
-            coffeeLayout.setBackgroundColor(
-                MyColors.getColorByAttr(
-                    requireContext(),
-                    R.attr.card_background,
-                    R.color.tab_background
-                )
-            )
-            burgerLayout.setBackgroundColor(
-                MyColors.getColorByAttr(
-                    requireContext(),
-                    R.attr.card_background,
-                    R.color.tab_background
-                )
-            )
-            mealLayout.setBackgroundColor(
-                MyColors.getColorByAttr(
-                    requireContext(),
-                    R.attr.card_background,
-                    R.color.tab_background
-                )
-            )
-
-            adCheck.visibility = View.INVISIBLE
-            candyCheck.visibility = View.INVISIBLE
-            chocolateCheck.visibility = View.INVISIBLE
-            coffeeCheck.visibility = View.INVISIBLE
-            burgerCheck.visibility = View.INVISIBLE
-            mealCheck.visibility = View.INVISIBLE
-
-            donateButton.isEnabled = true
+            for (sku in list){
+                try {
+                    when {
+                        sku.title.contains("burger", true) -> {
+                            burgerPrice.text = sku.price
+                            burgerCard.setOnClickListener {
+                                viewModel.purchaseItem(requireActivity(),sku)
+                            }
+                        }
+                        sku.title.contains("candy", true) -> {
+                            candyPrice.text = sku.price
+                            candyCard.setOnClickListener {
+                                viewModel.purchaseItem(requireActivity(),sku)
+                            }
+                        }
+                        sku.title.contains("coffee", true) -> {
+                            coffeePrice.text = sku.price
+                            coffeeCard.setOnClickListener {
+                                viewModel.purchaseItem(requireActivity(),sku)
+                            }
+                        }
+                        sku.title.contains("chocolate", true) -> {
+                            chocolatePrice.text = sku.price
+                            chocolateCard.setOnClickListener {
+                                viewModel.purchaseItem(requireActivity(),sku)
+                            }
+                        }
+                        sku.title.contains("dinner", true) -> {
+                            mealPrice.text = sku.price
+                            mealCard.setOnClickListener {
+                                viewModel.purchaseItem(requireActivity(),sku)
+                            }
+                        }
+                        else -> Unit
+                    }
+                }catch (e: Exception){
+                    Log.d("Log for - ","${e.message} \n\n\n\n\n ${e.printStackTrace()}")
+                }
+            }
         }
     }
 
--- app/src/main/java/com/baljeet/expirytracker/fragment/settings/donate/DonateViewModel.kt
+++ app/src/main/java/com/baljeet/expirytracker/fragment/settings/donate/DonateViewModel.kt
@@ -1,43 +1,88 @@
 package com.baljeet.expirytracker.fragment.settings.donate
 
+import android.app.Activity
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.android.billingclient.api.*
 import com.baljeet.expirytracker.CustomApplication
 import com.baljeet.expirytracker.util.Constants
-import com.baljeet.expirytracker.util.SharedPref
+import kotlinx.coroutines.launch
 
-class DonateViewModel(app : Application) : AndroidViewModel(app) {
-
+class DonateViewModel(app : Application): AndroidViewModel(app) {
     val context = getApplication<CustomApplication>()
 
-    private var donationAmount = 0.0
+    private var billingClient: BillingClient
+
+    val skuLiveList = MutableLiveData<MutableList<SkuDetails>>()
+
+    val purchaseComplete = MutableLiveData(false)
 
     init {
-        SharedPref.init(context)
+        val purchasesUpdatedListener = PurchasesUpdatedListener { billingResult, purchases ->
+            if(billingResult.responseCode == BillingClient.BillingResponseCode.OK ){
+                purchases?.let {
+                    purchaseComplete.postValue(true)
+                }
+            }else{
+                purchaseComplete.postValue(false)
+            }
+        }
+        billingClient = BillingClient.newBuilder(context)
+            .setListener(purchasesUpdatedListener)
+            .enablePendingPurchases()
+            .build()
+        connectToGooglePlayBilling()
+    }
+    fun purchaseItem(activity : Activity, sku: SkuDetails){
+        billingClient.launchBillingFlow(activity,
+            BillingFlowParams.newBuilder().setSkuDetails(sku)
+                .build() )
     }
 
 
-    fun getDonationAmount (): Double{
-        return donationAmount
+    private fun connectToGooglePlayBilling(){
+        viewModelScope.launch {
+            billingClient.startConnection(
+                object : BillingClientStateListener {
+                    override fun onBillingServiceDisconnected() {
+                        connectToGooglePlayBilling()
+                    }
+
+                    override fun onBillingSetupFinished(result: BillingResult) {
+                        if (result.responseCode == BillingClient.BillingResponseCode.OK){
+                            getProductDetails()
+                        }
+                    }
+                }
+            )
+        }
     }
 
-    fun selectAd(){
-        donationAmount = Constants.AD
+
+    private fun getProductDetails(){
+        val skuList = ArrayList<String>()
+        skuList.clear()
+        skuList.add(Constants.CANDY_ID)
+        skuList.add(Constants.CHOCOLATE_ID)
+        skuList.add(Constants.COFFEE_ID)
+        skuList.add(Constants.BURGER_ID)
+        skuList.add(Constants.DINNER_ID)
+        val productsDetailQuery =  SkuDetailsParams.newBuilder()
+            .setSkusList(skuList)
+            .setType(BillingClient.SkuType.INAPP)
+            .build()
+        billingClient.querySkuDetailsAsync(
+            productsDetailQuery
+        ) { result, list ->
+            if (result.responseCode == BillingClient.BillingResponseCode.OK) {
+                list?.let {
+                    skuLiveList.postValue(it)
+                }
+            }
+        }
     }
-    fun selectCandy(){
-        donationAmount = Constants.CANDY
-    }
-    fun selectChocolate(){
-        donationAmount = Constants.CHOCOLATE
-    }
-    fun selectCoffee(){
-        donationAmount = Constants.COFFEE
-    }
-    fun selectBurger(){
-        donationAmount = Constants.BURGER
-    }
-    fun selectMeal(){
-        donationAmount = Constants.MEAL
-    }
+
 
 }
